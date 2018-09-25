6dc2fc54d1dcb1f55859e5ed96e33133a09801af
Rebecca Hill <beerecca@gmail.com>
2018-09-25 14:53:22 +0200

    Tweaks

--- bash/.bash/wetransfer.sh
+++ bash/.bash/wetransfer.sh
@@ -1,7 +1,13 @@
+# Ruby
 export RBENV_VERSION=ruby-2.3.4
 eval "$(rbenv init -)"
+alias be="bundle exec"
 
+# MySQL
 export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
 
+# Terraform
+export PATH=$PATH:/Users/rebecca/
+
+# Ngrok
 alias ngrok="~/ngrok"
-alias be="bundle exec"
